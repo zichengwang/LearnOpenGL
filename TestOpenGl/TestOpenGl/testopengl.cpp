52b1ec092d088e3d44212403a5aa7ccf4f23bdde
zichengwang <siriuswang235@sina.cn>
2018-12-06 22:17:48 +0800

    shader class

--- TestOpenGl/TestOpenGl/testopengl.cpp
+++ TestOpenGl/TestOpenGl/testopengl.cpp
@@ -10,6 +10,9 @@ void processInput(GLFWwindow *window);
 int main()
 {
 	unsigned int VBO;  //定点缓冲对象
+	unsigned int VAO;  //定点缓冲对象数组
+	unsigned int EBO;  //索引缓冲对象
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -38,18 +41,38 @@ int main()
 
 	//定点数据数组
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,   // 右上角
+		0.5f, -0.5f, 0.0f,  // 右下角
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f   // 左上角
 	};
 
+	unsigned int indices[] = { // 注意索引从0开始! 
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+
+	//绑定VAO 	
+	glBindVertexArray(VAO);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); //将定点缓冲对象VBO绑定到GL_ARRAY_BUFFER,
 	                                    //绑定后，所有对GL_ARRAY_BUFFER的操作都会用来配置
 	                                    //VBO
-
 	//将顶点数据复制到顶点缓冲对象中
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//指定如何解析顶点数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);  //启用顶点属性，默认是禁止的
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //线框模式
 
 	while (!glfwWindowShouldClose(window))
 	{
